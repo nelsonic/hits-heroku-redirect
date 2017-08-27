450a757f7d024edab011062c37286da7d2adf2ec
nelsonic <contact.nelsonic+github@gmail.com>
2017-08-27 08:01:31 +0100

    update url to use subdomain http://hits.dwyl.io fixes https://github.com/dwyl/hits/issues/22

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var port  = process.env.PORT || 8000;
 var fs    = require('fs'); // so we can open the HTML & JS file
 var app = require('http').createServer(handler)
-var NEW_SERVER = 'http://178.79.141.232'
+var NEW_SERVER = 'http://hits.dwyl.io'
 
 function handler (req, res) {
   var url = req.url;
