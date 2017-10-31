aea2396cb2e2570fe59fdd8f56a450cc84fa1445
Ebuka Hills <flyg101@gmail.com>
2017-10-31 22:26:52 +0100

    refactor lanip.js with map and filter functions

--- lanip.js
+++ lanip.js
@@ -1,16 +1,14 @@
 /**
- * If you want to now the IP Address on the Local Network you're in luck!
+ * If you want to know the IP Address on the Local Network you're in luck!
  * see/credit: http://stackoverflow.com/questions/10750303
  */
 var os = require('os');
 var interfaces = os.networkInterfaces();
-var ip = [];
-for (var k in interfaces) {
-  for (var k2 in interfaces[k]) {
-    var address = interfaces[k][k2];
-    if (address.family === 'IPv4' && !address.internal) {
-      ip.push(address.address);
-    }
-  }
-}
-module.exports = ip[0];
+
+var ip = Object.keys(interfaces)
+  .map(i =>interfaces[i].
+      filter(address => address.family === 'IPv4' && !address.internal)[0]
+  )
+  .filter(i => i)[0].address;
+
+module.exports = ip;
